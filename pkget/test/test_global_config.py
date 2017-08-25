ee83ca2fd81d98773579b67b9370c2f929aec21a
miacro <fqguozhou@gmail.com>
2017-08-25 10:32:34 +0800

    update test_global_config
    *

--- pkget/test/test_global_config.py
+++ pkget/test/test_global_config.py
@@ -1,19 +1,33 @@
 import unittest
-from pkget import GlobalConfig
+import os
+from pkget import GlobalConfig, Utils
 
 
 class GlobalConfigTest(unittest.TestCase):
     def test_update_config(self):
         config = GlobalConfig()
-        print("default")
-        print(config)
+        recipepaths = \
+            [os.path.join(
+                os.path.dirname(os.path.dirname(__file__)),  "recipes")]
+        self.assertEqual(config.recipepaths, recipepaths)
+        self.assertIs(config.installprefix, "")
+        self.assertIs(config.pkginfoprefix, "")
         config.update_config({
             "installprefix": "~/.local",
             "pkginfoprefix": "~",
             "recipepaths": ["~/.local", "", None],
         })
-        print(config)
+        recipepaths.append(Utils.abspath("~/.local"))
+
+        self.assertEqual(config.recipepaths, recipepaths)
+        self.assertEqual(config.installprefix, Utils.abspath("~/.local"))
+        self.assertEqual(config.pkginfoprefix, Utils.abspath("~"))
+
         config.update_config({
-            "installprefix": None,
+            "installprefix": "",
+            "pkginfoprefix": None,
+            "recipepaths": ["", None],
         })
-        print(config)
+        self.assertEqual(config.recipepaths, recipepaths)
+        self.assertEqual(config.installprefix, Utils.abspath("~/.local"))
+        self.assertEqual(config.pkginfoprefix, Utils.abspath("~"))
